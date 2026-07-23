659623b974c1432ad252d881e7bf695223876cff
Aymane El Halkouj <aymel-ha@e3r1p1.1337.ma>
2026-07-23 05:43:48 +0100

    RAG_AM: starting...

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,18 @@
-from fire import Fire
+import fire
+from .starting_point import printi
+
+def index(max_chunk_size: int):
+    pass
+def search(query, k: int):
+    pass
+def search_dataset(dataset_path: str, k:int, save_directory: str):
+    pass
+def answer(query, k: int):
+    pass
+def answer_dataset(student_search_results_path: str, save_directory: str):
+    pass
+def evaluate(student_search_results_path: str, dataset_path: str):
+    pass
 
 if __name__ == '__main__':
-    Fire()
+    fire.Fire()
--- src/starting_point.py
+++ src/starting_point.py
@@ -1,21 +1,3 @@
-import fire
-import sys
-def hello(name):
-  return f'Hello {name}!'
+def printi(s):
+    return s
 
-class A:
-  def __init__(self, name):
-    self.name = name
-  def salute(self, sue):
-    print(f'{sue}\n')
-    return self
-  def greet(self, jj):
-    print(f'{jj}\n')
-    return self
-  def to_stop(self):
-    pass
-  def __str__(self):
-    return f'hello {self.name}'
-
-if __name__ == '__main__':
-  fire.Fire()
