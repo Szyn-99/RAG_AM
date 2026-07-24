dfbf18a1df26abe5e591b3a8479e1107b05e4103
Aymane El Halkouj <aymel-ha@e3r1p7.1337.ma>
2026-07-24 15:00:39 +0100

    RAG_AM: starting...

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,5 @@
 import fire
-from .starting_point import printi
+from os import scandir
 
 def index(max_chunk_size: int):
     pass
@@ -13,6 +13,15 @@ def answer_dataset(student_search_results_path: str, save_directory: str):
     pass
 def evaluate(student_search_results_path: str, dataset_path: str):
     pass
+
+
+def scan(directory_path: str):
+    file_paths = set()
+    for entry in scandir(directory_path):
+        if entry.is_file():
+            file_paths.add(entry.path)
+
+    print(file_paths[0].split)
 
 if __name__ == '__main__':
     fire.Fire()
