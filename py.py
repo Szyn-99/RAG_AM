f9f5185527628a97b51212eb7fd96e388c8de304
Aymane El Halkouj <aymel-ha@e3r1p9.1337.ma>
2026-08-06 22:28:10 +0100

    RAG_AM: starting...

--- py.py
+++ py.py
@@ -1,8 +1,33 @@
+code = """
+import os
 import ast
 
-source = "def func(x) -> x:\n\treturn x ** 2"
-tree = ast.parse(source)
+PI = 3.14
 
-print(tree)
-print(ast.dump(tree, indent=4))
-print(int('0928', base=7))
+class Parser:
+    ...
+
+def tokenize(x):
+    ...
+
+def parse():
+    ...
+
+if __name__ == "__main__":
+    ..."""
+
+from ast import parse, dump, get_source_segment
+
+tree = parse(code)
+# print(tree._fields)
+# print(tree.body[4].args._fields)
+# print(get_source_segment(code, tree.body[0]), tree.body[0].col_offset, tree.body[0].end_col_offset+1)
+# for node in tree.body:
+#     print('source',get_source_segment(code, node))
+#     print('len',len(get_source_segment(code, node)))
+
+#     print('start:', node.col_offset)
+#     print('start:', node.end_col_offset)
+#     print('='*40)
+
+print("import os"[0:9])
